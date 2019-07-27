ae5dd1a95ffc78a1ab0f7d6a9ee276706e422bcd
yongkang.li <yongkang.li@zhangmen.com>
2019-07-27 18:23:03 +0800

    firstTriangle

---
+++ CMake/firstWindow/build/CMakeFiles/firstWindow.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstWindow.dir/main.cpp.obj"
+  "firstWindow.exe"
+  "firstWindow.exe.manifest"
+  "firstWindow.pdb"
+  "libfirstWindow.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firstWindow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
